17dabe796b5934de71f0926ffa316355fc4a1b3e
aleneri <alessandro.neri@gmail.com>
2022-07-12 15:42:11 -0300

    feat: add route /me

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -4,9 +4,11 @@ import { DetailUserService } from "../../services/user/DetailUserService";
 class DetailUserController{
 
     async handle(req: Request, res: Response){
+        const user_id = req.user_id;
+        console.log("ID DO USER: ", user_id);
         const {name, email, password} = req.body;
         const service = new DetailUserService();
-        const result = await service.execute();
+        const result = await service.execute(user_id);
         return res.json(result);
     }
 
